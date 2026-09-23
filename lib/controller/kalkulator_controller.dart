8be67bde35e8adb422c23940ee5b4f965ce8c583
Aji Suryawan <kazumovie132@gmail.com>
2026-09-23 11:32:32 +0700

    getx 1

---
+++ lib/controller/kalkulator_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class KalkulatorController extends GetxController {
+  var hasilHitung = 0.0.obs;
+
+  // method tambah kurang kali dan bagi
+  void tambah(double angka1, double angka2) {
+    double hasilTambah = angka1 + angka2;
+    hasilHitung.value = hasilTambah;
+  }
+
+  void kurang(double angka1, double angka2) {
+    double hasilTambah = angka1 - angka2;
+    hasilHitung.value = hasilTambah;
+  }
+
+  void kali(double angka1, double angka2) {
+    double hasilTambah = angka1 * angka2;
+    hasilHitung.value = hasilTambah;
+  }
+}
--- lib/kalkulator_page.dart
+++ lib/kalkulator_page.dart
@@ -1,15 +1,48 @@
+import 'package:belajarflutter11pplg3/components/my_textfield.dart';
+import 'package:belajarflutter11pplg3/controller/kalkulator_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class KalkulatorPage extends StatefulWidget {
-  const KalkulatorPage({super.key});
+class KalkulatorPage extends StatelessWidget {
+  KalkulatorPage({super.key});
 
-  @override
-  State<KalkulatorPage> createState() => _KalkulatorPageState();
-}
-
-class _KalkulatorPageState extends State<KalkulatorPage> {
+  final controller = Get.put(KalkulatorController());
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController txtangka1 = TextEditingController();
+    TextEditingController txtangka2 = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(title: Text("Kalkulator")),
+      body: Column(
+        children: [
+          MyTextfield(
+            myHint: "input angka 1",
+            txtController: txtangka1,
+            radius: 10,
+          ),
+          MyTextfield(
+            myHint: "input angka 2",
+            txtController: txtangka2,
+            radius: 10,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              controller.tambah(
+                double.parse(txtangka1.text.toString()),
+                double.parse(txtangka2.text.toString()),
+              );
+            },
+            child: Text("tambah"),
+          ),
+
+          Obx(
+            () => Text(
+              controller.hasilHitung.toString(),
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
